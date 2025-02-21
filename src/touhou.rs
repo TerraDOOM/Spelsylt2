403221ec7b2a8ed0bce1d70fdf6f42dd4f621681
Theodor Ström <thstro@kth.se>
2025-02-21 21:40:02 +0100

    Started working on touhou.rs

--- src/touhou.rs
+++ src/touhou.rs
@@ -1,16 +1,23 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
+use crate::xcom;
+
+#[derive(Component, Default)]
 struct TouhouMarker;
+#[derive(Component, Default)]
+struct PlayerMarker;
 
 pub fn touhou_plugin(app: &mut App) {
     app.add_plugins((RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0)))
         .add_systems(Startup, setup);
 }
 
-pub fn setup(mut commands: Commands) {}
+pub fn setup(mut commands: Commands) {
+    xcom::setup;
+}
 
-#[derive(Bundle)]
+#[derive(Bundle, Default)]
 pub struct Player {
     controller: KinematicCharacterController,
     sprite: Sprite,
@@ -18,4 +25,21 @@ pub struct Player {
     trans: Transform,
     collider: Collider,
     coll_types: ActiveCollisionTypes,
+    markers: (PlayerMarker, TouhouMarker)
+}
+
+pub fn spawn_player(mut commands: Commands, mut rapier_config: Query<&mut RapierConfiguration>, asset_server: ResMut<AssetServer>) {
+    let mut rapier_config = rapier_config.single_mut();
+    rapier_config.gravity = Vec2::ZERO;
+
+    commands.spawn(Player {
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(10.0, 10.0)),
+            image: asset_server.load("player.png"),
+            ..Default::default()
+        }
+        trans: Transform::from_xyz(0, 0, 0),
+        rb: RigidBody::KinematicPositionBased,
+        coll_types: ActiveCollisionTypes::KINEMATIC_STATIC,
+    })
 }
