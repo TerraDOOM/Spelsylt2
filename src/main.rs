80aa8187ba456c8eeb5f8000116c2a145ca8c43c
Theodor Ström <thstro@kth.se>
2025-02-21 22:03:42 +0100

    Added a camera

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use bevy::{prelude::*, winit::WinitSettings};
 
-mod touhou;
-mod xcom;
-mod types;
 mod prelude;
+mod touhou;
+mod types;
+mod xcom;
 
 #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
 enum GameState {
@@ -18,5 +18,10 @@ fn main() {
         .insert_resource(WinitSettings::desktop_app())
         .add_plugins(xcom::xcom_plugin)
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Setup, global_setup)
         .run();
+}
+
+pub fn global_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default())
 }
