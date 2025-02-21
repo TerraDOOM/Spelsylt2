38c4f9a38cbb62990e4cced0eef5746d20d4c4db
Theodor Ström <thstro@kth.se>
2025-02-21 21:53:02 +0100

    More work on touhou

--- src/touhou.rs
+++ src/touhou.rs
@@ -13,9 +13,7 @@ pub fn touhou_plugin(app: &mut App) {
         .add_systems(Startup, setup);
 }
 
-pub fn setup(mut commands: Commands) {
-    xcom::setup;
-}
+pub fn setup(mut commands: Commands) {}
 
 #[derive(Bundle, Default)]
 pub struct Player {
@@ -25,21 +23,29 @@ pub struct Player {
     trans: Transform,
     collider: Collider,
     coll_types: ActiveCollisionTypes,
-    markers: (PlayerMarker, TouhouMarker)
+    markers: (PlayerMarker, TouhouMarker),
 }
 
-pub fn spawn_player(mut commands: Commands, mut rapier_config: Query<&mut RapierConfiguration>, asset_server: ResMut<AssetServer>) {
+#[derive(Bundle, Default)]
+pub struct Bullet {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    mut rapier_config: Query<&mut RapierConfiguration>,
+    asset_server: ResMut<AssetServer>,
+) {
     let mut rapier_config = rapier_config.single_mut();
     rapier_config.gravity = Vec2::ZERO;
 
     commands.spawn(Player {
         sprite: Sprite {
             custom_size: Some(Vec2::new(10.0, 10.0)),
-            image: asset_server.load("player.png"),
+            image: asset_server.load("mascot.png"),
             ..Default::default()
-        }
-        trans: Transform::from_xyz(0, 0, 0),
+        },
+        trans: Transform::from_xyz(0.0, 0.0, 0.0),
         rb: RigidBody::KinematicPositionBased,
         coll_types: ActiveCollisionTypes::KINEMATIC_STATIC,
-    })
+        ..Default::default()
+    });
 }
