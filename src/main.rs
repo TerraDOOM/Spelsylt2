276c02b00882ba3bc1af938c4ecab32af9ea9370
Theodor Ström <thstro@kth.se>
2025-02-21 20:29:21 +0100

    More skeleton code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use bevy::{prelude::*, winit::WinitSettings};
 mod touhou;
 mod xcom;
 
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum GameState {
+    #[default]
+    Menu,
+    Xcom,
+    Touhou,
+}
+
 fn main() {
     App::new()
         .insert_resource(WinitSettings::desktop_app())
--- src/touhou.rs
+++ src/touhou.rs
@@ -4,8 +4,11 @@ use bevy_rapier2d::prelude::*;
 struct TouhouMarker;
 
 pub fn touhou_plugin(app: &mut App) {
-    app.add_plugins((RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0)));
+    app.add_plugins((RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0)))
+        .add_systems(Startup, setup);
 }
+
+pub fn setup(mut commands: Commands) {}
 
 #[derive(Bundle)]
 pub struct Player {
