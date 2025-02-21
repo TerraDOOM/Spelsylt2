8802d3daba1f1a0a3141635d1b624b55a997a52c
Theodor Ström <thstro@kth.se>
2025-02-21 22:18:41 +0100

    I forgot the semicolon

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,10 @@ fn main() {
         .insert_resource(WinitSettings::desktop_app())
         .add_plugins(xcom::xcom_plugin)
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_systems(Setup, global_setup)
+        .add_systems(Startup, global_setup)
         .run();
 }
 
 pub fn global_setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default())
+    commands.spawn(Camera2dBundle::default());
 }
